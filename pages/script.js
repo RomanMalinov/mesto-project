fba3fac10977bb7b79dad6864924d283e4744676
Roman <malinovroman@mail.ru>
2023-04-20 17:42:43 +0300

    сделана HTML разметка и подключил JS

---
+++ pages/script.js
@@ -0,0 +1,16 @@
+console.log('test');
+
+//1. Работа модальных окон
+// Открытие и закрытие модального окна
+
+
+// Удаление карточки пока
+//  const deletButtonCard = document.querySelector('.element__delete-button');
+//  deletButtonCard.addEventListener('click', function () {
+//  const listItem = deletButtonCard.closest('.element');
+//  listItem.remove();
+//  });
+
+
+
+///
