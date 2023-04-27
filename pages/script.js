c09544b0a7894d20c7f8068fca565690623b7631
Roman <malinovroman@mail.ru>
2023-04-28 01:44:29 +0300

    Исправил не обязательные исправления ревью

--- pages/script.js
+++ pages/script.js
@@ -1,6 +1,28 @@
-console.log('test');
+// Переменные
+const listContainerEl = document.querySelector('.elements');
+const temlateEl = document.querySelector('.template')
+// Открытие и закрытие модальных окон
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupAddNewCards = document.querySelector('.popup_type_add_new-cards');
+const profileInfoEditButton = document.querySelector('.profile__info-edit-button');
+const profileAddCardsButton = document.querySelector('.profile__add-button');
+// Открытие попапа с картинкой
+const popupCards = document.querySelector('.popup_type_zoom-card');
+const popupCardsImage = popupCards.querySelector('.popup__container-img');
+const popupCardsText = popupCards.querySelector('.popup__container-text');
+// Все кнопки закрытия модальных окон
+const closeButtons = document.querySelectorAll('.popup__close');
+// Редактирование имени и информации о себе
+const formPopupProfile = document.forms['profile-form'];
+// Находим поля формы в DOM
+const popupProfileNameInput = document.querySelector('.popup__form-input-name');
+const popupProfilejobInput = document.querySelector('.popup__form-input-job');
+//Форма добавления карточки
+const formPopupNewCards = document.forms['card-form'];
+const popupNewCardsNameInput = document.querySelector('.popup__form-input-img-name');
+const popupNewCardsLinkInput = document.querySelector('.popup__form-input-link')
 
-// Ренднринг карточек
+// Рендеринг карточек
 const initialCards = [
   {
     name: 'Ландыши',
@@ -28,8 +50,6 @@ const initialCards = [
     link: './images/element__img-cat-6.jpg',
   }
 ];
-const listContainerEl = document.querySelector('.elements');
-const temlateEl = document.querySelector('.template');
 
 function renderCards(addCard) {
   const newCards = initialCards.map(addCard);
@@ -61,87 +81,60 @@ function addCard(item) {
   }
   // слушатель функция открытия модального окна по клику на картинку элемента
   // функция открытия модального окна по клику на картинку элемента
-
   function imageClickHandler() {
     popupCardsImage.src = item.link;
     popupCardsText.textContent = item.name;
     popupCardsImage.alt = item.name;
-    togglePopup(popupCards)
+    openPopup(popupCards);
   }
-
   imageEl.addEventListener('click', imageClickHandler)
   return newItem;
 }
 renderCards(addCard);
 
-// Открытие и закрытие модальных окон
-const popupProfile = document.querySelector('.popup_type_profile');
-const popupAddNewCards = document.querySelector('.popup_type_add_new-cards');
-const profileInfoEditButton = document.querySelector('.profile__info-edit-button');
-const profileAddCardsButton = document.querySelector('.profile__add-button');
-const popupProfileCloseButtone = popupProfile.querySelector('.popup__button-close');
-const profileAddCardCloseButton = popupAddNewCards.querySelector('.popup__button-close');
-
-// Открытие попапа с картинкой
-const popupCards = document.querySelector('.popup_type_zoom-card');
-const popupCardsCloseButtone = popupCards.querySelector('.popup__button-close');
-const popupCardsImage = popupCards.querySelector('.popup__container-img');
-const popupCardsText = popupCards.querySelector('.popup__container-text');
-
-// единая функция открытия и закрытия модаьных окон
-function togglePopup(popupElement) {
-  popupElement.classList.toggle('popup_opened')
+// функции открытия и закрытия модальных окон
+// функции закрытия
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+// функции открытия
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-profileInfoEditButton.addEventListener('click', () => togglePopup(popupProfile));
-profileAddCardsButton.addEventListener('click', () => togglePopup(popupAddNewCards));
-popupProfileCloseButtone.addEventListener('click', () => togglePopup(popupProfile));
-profileAddCardCloseButton.addEventListener('click', () => togglePopup(popupAddNewCards));
-popupCardsCloseButtone.addEventListener('click', () => togglePopup(popupCards));
+console.log(closeButtons);
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап
+  const popup = button.closest('.popup');
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener('click', () => closePopup(popup));
+});
 
-// Редактирование имени и информации о себе
-const formPopupProfile = popupProfile.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-const popupProfileNameInput = document.querySelector('.popup__form-input-name');// Воспользуйтесь инструментом .querySelector()
-const popupProfilejobInput = document.querySelector('.popup__form-input-job')// Воспользуйтесь инструментом .querySelector()
+//Функции открытия модальных окон
+profileInfoEditButton.addEventListener('click', () => openPopup(popupProfile));
+profileAddCardsButton.addEventListener('click', () => openPopup(popupAddNewCards));
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Обработчик «отправки» формы
 function handleProfileFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  evt.target.reset();
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
   const nameInputForm = document.querySelector('.profile__info-title');
   const jobInputForm = document.querySelector('.profile__info-subtitle');
   nameInputForm.textContent = popupProfileNameInput.value;
   jobInputForm.textContent = popupProfilejobInput.value;
-  // Получите значение полей jobInput и nameInput из свойства value
-  // Выберите элементы, куда должны быть вставлены значения полей
-  // Вставьте новые значения с помощью textContent
-  togglePopup(popupProfile)
+  evt.target.reset();
+  closePopup(popupProfile)
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formPopupProfile.addEventListener('submit', handleProfileFormSubmit);
 
-//Форма добавления карточки
-const formPopupNewCards = document.querySelector('.popup__form_type_new-card');
-const popupNewCardsNameInput = document.querySelector('.popup__form-input-img-name');
-const popupNewCardsLinkInput = document.querySelector('.popup__form-input-link')
-
+//Функции открытия модальных окон
 function handleFormAddNewCard(evt) {
   evt.preventDefault();
-  evt.target.reset();
   const newName = popupNewCardsNameInput.value;
   const newLink = popupNewCardsLinkInput.value;
   const newCard = addCard({ name: newName, link: newLink });
   listContainerEl.prepend(newCard)
-
-  togglePopup(popupAddNewCards)
+  evt.target.reset();
+  closePopup(popupAddNewCards);
 }
 formPopupNewCards.addEventListener('submit', handleFormAddNewCard);
-
-
 
