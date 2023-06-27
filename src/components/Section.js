5d35d1a63e634895c6eec400691acecf79d07890
Kinox19 <puchkouartem@gmail.com>
2023-06-27 13:43:26 +0300

    Реализация классов Section и Card: описание конструкторов и методов классов

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,13 @@
+import {listContainerEl} from '../components/constants'
+
 export default class Section{
     constuctor({renderer}, selectors){
         this._selectors = selectors;
         this._renderer = renderer;
+        this.container = listContainerEl;
+    }
+
+    addCard(item){
+        this.container.prepend(this._renderer(item))
     }
 }
